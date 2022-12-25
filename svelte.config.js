f7380fea337069c2b2ca6e65b4af84964e3fbc95
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-12-25 20:08:55 +0800

    build: sveltekit config

--- svelte.config.js
+++ svelte.config.js
@@ -12,38 +12,27 @@ const config = {
         methodOverride: {
             allowed: ["PATCH", "DELETE"],
         },
-        package: {
-            files: (filepath) => {
-                const [lib] = filepath.split("/");
-                return lib === "seeder";
-            },
-            emitTypes: false,
+        alias: {
+            $lib: resolve("src/lib"),
+            $actions: resolve("src/lib/actions"),
+            $components: resolve("src/lib/components"),
+            $config: resolve("src/lib/config"),
+            $enums: resolve("src/lib/enums"),
+            $features: resolve("src/lib/features"),
+            $stores: resolve("src/lib/stores"),
+            $layouts: resolve("src/lib/layouts"),
+            $types: resolve("src/lib/types"),
+            $utils: resolve("src/lib/utils"),
+            $validations: resolve("src/lib/validations"),
+            $routes: resolve("src/routes"),
+            $_model: resolve("src/routes/_model"),
+            $_collection: resolve("src/routes/_collection"),
         },
         vite: {
             server: {
-                fs: {
-                    allow: [".."],
-                },
-            },
-            optimizeDeps: {
-                include: ["lodash-es"],
-            },
-            resolve: {
-                alias: {
-                    $lib: resolve("src/lib"),
-                    $actions: resolve("src/lib/actions"),
-                    $components: resolve("src/lib/components"),
-                    $config: resolve("src/lib/config"),
-                    $enums: resolve("src/lib/enums"),
-                    $features: resolve("src/lib/features"),
-                    $stores: resolve("src/lib/stores"),
-                    $layouts: resolve("src/lib/layouts"),
-                    $types: resolve("src/lib/types"),
-                    $utils: resolve("src/lib/utils"),
-                    $validations: resolve("src/lib/validations"),
-                    $routes: resolve("src/routes"),
-                    $_model: resolve("src/routes/_model"),
-                    $_collection: resolve("src/routes/_collection"),
+                cors: {
+                    allowedHeaders: ["Set-Cookie"],
+                    origin: process.env.APP_URL,
                 },
             },
             envPrefix: "SVELTE_",
