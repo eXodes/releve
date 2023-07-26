9c40a512bd8544520655d7357a4056e349c2c48f
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-07-27 00:52:47 +0800

    build: change to adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
