ad5f7cdbf5ba2f2671b10cbb8c1d980f6b7f623e
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-07-29 03:40:57 +0800

    chore: log admin

--- src/lib/server/services/firebase-admin.ts
+++ src/lib/server/services/firebase-admin.ts
@@ -2,6 +2,7 @@ import { env } from "$env/dynamic/private";
 import admin from "firebase-admin";
 import { type App, applicationDefault, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { readFile } from "fs";
 
 let app: App | undefined;
 
@@ -17,11 +18,25 @@ if (app) {
     });
 }
 
+readFile(env.GOOGLE_APPLICATION_CREDENTIALS as string, (err, data) => {
+    if (err) {
+        // eslint-disable-next-line no-console
+        console.error("[Service Account Error]: ", err);
+    }
+
+    // eslint-disable-next-line no-console
+    console.log("[Service Account]: ", data.toString());
+});
+
 // eslint-disable-next-line no-console
-console.log("[Service Account]: ", env.GOOGLE_APPLICATION_CREDENTIALS);
+console.log("[Service Account]: ");
 // eslint-disable-next-line no-console
 console.log("[Firebase Admin]: ", admin.apps);
 // eslint-disable-next-line no-console
 console.log("[Firebase Admin Initialized]: ", app);
+
+// read from filesystem
+
+// read from env
 
 export default app;
