d2f4c7941d511801b5a105b3c7db9271c0ef94d9
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-07-29 01:56:18 +0800

    fix: initialise firebase with cert

--- src/lib/server/services/firebase-admin.ts
+++ src/lib/server/services/firebase-admin.ts
@@ -1,12 +1,13 @@
+import { env } from "$env/dynamic/private";
 import admin from "firebase-admin";
-import { type App, applicationDefault, initializeApp } from "firebase-admin/app";
+import { type App, cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 let app: App | undefined;
 
 if (!admin.apps.length) {
     app = initializeApp({
-        credential: applicationDefault(),
+        credential: cert(env.GOOGLE_APPLICATION_CREDENTIALS),
     });
 }
 
