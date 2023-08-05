fa5c2f70c3c81338dfbd05ca9497673dde22f4cf
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-06 01:25:35 +0800

    build: enable web frameworks

--- src/lib/server/services/firebase-admin.ts
+++ src/lib/server/services/firebase-admin.ts
@@ -1,13 +1,18 @@
 import admin from "firebase-admin";
-import { type App, applicationDefault, initializeApp } from "firebase-admin/app";
+import { type App, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 let app: App | undefined;
 
 if (!admin.apps.length) {
-    app = initializeApp({
-        credential: applicationDefault(),
-    });
+    console.info("[FIREBASE_ADMIN]: Initializing Firebase Admin SDK...");
+    app = initializeApp();
+} else {
+    const [defaultApp] = admin.apps;
+
+    console.info("[FIREBASE_ADMIN]: Firebase Admin SDK already initialized:", defaultApp);
+
+    app = defaultApp ?? initializeApp();
 }
 
 if (app) {
