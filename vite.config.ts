52efec71ce55fe13fa62ba8d61145ba832e1b725
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-07 00:26:40 +0800

    build: sentry deployment env and set commits

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, loadEnv } from "vite";
 
 const config = defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "SENTRY_");
+    const env = loadEnv(mode, process.cwd(), "");
 
     return {
         plugins: [
             sentrySvelteKit({
                 sourceMapsUploadOptions: {
                     telemetry: false,
+                    deploy: {
+                        env: env.PUBLIC_APP_ENV,
+                    },
+                    setCommits: {
+                        auto: true,
+                    },
                 },
                 autoUploadSourceMaps: env.SENTRY_RELEASE !== undefined,
             }),
