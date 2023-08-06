0f8405b35d74aebc9af9b3726be1aeba80d55e5c
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-07 01:29:08 +0800

    chore: cleanup imports

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,15 @@
-import { env } from "$env/dynamic/public";
-import { PUBLIC_APP_ENV } from "$env/static/public";
-import { sequence } from "@sveltejs/kit/hooks";
-import * as Sentry from "@sentry/sveltekit";
 import "dotenv/config";
 
-import { SESSION_COOKIE } from "$server/utils/cookie";
-import { AuthService } from "$module/auth/auth.service";
-import { getCookieValue } from "$client/utils/endpoint";
-import "$server/services/firebase-admin";
+import { env } from "$env/dynamic/public";
+import { PUBLIC_APP_ENV } from "$env/static/public";
 
-import type { Handle, HandleServerError } from "@sveltejs/kit";
+import "$server/services/firebase-admin";
+import { getCookieValue, SESSION_COOKIE } from "$server/utils/cookie";
+import { AuthService } from "$module/auth/auth.service";
+
+import type { HandleServerError } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 
 Sentry.init({
     environment: PUBLIC_APP_ENV,
@@ -17,7 +17,7 @@ Sentry.init({
     tracesSampleRate: Number(env.PUBLIC_SENTRY_TRACE_RATE),
 });
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
+export const handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
     try {
         event.locals.session = await AuthService.verifySession(
             getCookieValue(event.request.headers, SESSION_COOKIE)
