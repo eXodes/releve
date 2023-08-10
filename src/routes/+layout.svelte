b448adc7558f5242709f2018271a141256f4890b
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-11 00:13:46 +0800

    feat: pwa manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import "../app.css";
+    import { pwaInfo } from "virtual:pwa-info";
+
+    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
+
+<svelte:head>
+    {@html webManifestLink}
+</svelte:head>
 
 <slot />
