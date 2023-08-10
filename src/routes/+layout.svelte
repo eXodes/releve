e5d0ee6f877c3abc7c0c7db4499846f9fa494ac1
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-11 00:32:29 +0800

    fix: register service worker when load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
     import "../app.css";
+
+    import { onMount } from "svelte";
     import { pwaInfo } from "virtual:pwa-info";
 
     $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
+    onMount(async () => {
+        if (pwaInfo) {
+            const { registerSW } = await import("virtual:pwa-register");
+            registerSW({
+                immediate: true,
+            });
+        }
+    });
 </script>
 
 <svelte:head>
