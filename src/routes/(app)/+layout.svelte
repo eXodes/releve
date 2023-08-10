a32e59a1d6913e778d181f39e4db9b19cb42ae42
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-10 22:51:07 +0800

    fix: app layouts

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-    import { invalidate } from "$app/navigation";
-
     import BaseLayout from "$client/components/layouts/base-layout.svelte";
     import Banner, { type Content } from "$client/components/shared/banner.svelte";
-    import Modal from "$client/components/shared/modal.svelte";
     import Toast from "$client/components/shared/toast.svelte";
-    import ShopForm from "$features/shops/components/shop-form.svelte";
 
     import { ExclamationTriangle } from "svelte-hero-icons";
 
@@ -17,21 +13,9 @@
         desktop: "This is a pre-release application. Use at your own risk.",
         mobile: "This is a pre-release application.",
     };
-
-    let showShopForm = false;
-
-    const handleShopSubmit = async () => {
-        showShopForm = false;
-
-        await invalidate("shops");
-    };
 </script>
 
-<Modal open={showShopForm} padding={false} on:close={() => (showShopForm = false)} static>
-    <ShopForm actionType="create" on:success={handleShopSubmit} />
-</Modal>
-
-<BaseLayout on:click={() => (showShopForm = true)}>
+<BaseLayout>
     <slot />
 
     {#if !data.session.authenticated}
