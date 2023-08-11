60f087b4913cc02fc216ba1678fc66af598f6883
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-08-12 06:26:04 +0800

    fix: standardised pages title and description

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -12,5 +12,16 @@
 </svelte:head>
 
 <Container>
-    <ShopGrid shops={data.shops} meta={data.meta} />
+    <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+            <h1 class="sr-only">Shops</h1>
+            <p class="mt-2 text-sm text-gray-700">
+                Curated list of shops that offers fast or same-day delivery.
+            </p>
+        </div>
+    </div>
+
+    <div class="mt-8">
+        <ShopGrid shops={data.shops} meta={data.meta} />
+    </div>
 </Container>
--- src/routes/(app)/my/shops/+page.svelte
+++ src/routes/(app)/my/shops/+page.svelte
@@ -12,5 +12,16 @@
 </svelte:head>
 
 <Container>
-    <ShopGrid isPrivate={true} shops={data.shops} meta={data.meta} />
+    <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+            <h1 class="sr-only">My Shops</h1>
+            <p class="mt-2 text-sm text-gray-700">
+                List of all the shops you have created as your private collection.
+            </p>
+        </div>
+    </div>
+
+    <div class="mt-8">
+        <ShopGrid isPrivate={true} shops={data.shops} meta={data.meta} />
+    </div>
 </Container>
