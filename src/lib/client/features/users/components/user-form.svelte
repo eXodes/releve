b895dcd6e2208498cca79387a15422c3df0a9d4e
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-12-22 17:09:10 +0800

    fix: user photo default

--- src/lib/client/features/users/components/user-form.svelte
+++ src/lib/client/features/users/components/user-form.svelte
@@ -40,6 +40,7 @@
             displayName: userData.displayName,
             role: userData.customClaims?.isAdmin ? Role.ADMIN : Role.USER,
             about: userData.about ?? "",
+            userPhoto: userData.avatar.medium.url ?? "",
             firstName: userData.information?.firstName ?? "",
             lastName: userData.information?.lastName ?? "",
             email: userData.email ?? "",
@@ -93,6 +94,7 @@
     $: showRoleInput = actionType === "users" && $page.data.session.user?.customClaims?.isAdmin;
 
     $: userPhoto = (() => {
+        console.log("userPhoto", $form.data.userPhoto);
         if ($form.data.userPhoto instanceof File) {
             return URL.createObjectURL($form.data.userPhoto);
         }
