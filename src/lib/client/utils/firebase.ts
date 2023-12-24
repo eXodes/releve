a69496007449313bf4629d5f2d8cc83a425cdaa7
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-12-25 06:16:03 +0800

    refactor: init app check in firebase client utils

--- src/lib/client/utils/firebase.ts
+++ src/lib/client/utils/firebase.ts
@@ -1,8 +1,11 @@
-import { dev } from "$app/environment";
+import { browser, dev } from "$app/environment";
 
 import { firebaseConfig, firebaseEmulator } from "$client/config/firebase";
+import { env } from "$env/dynamic/public";
+import { type AppCheck } from "firebase/app-check";
 
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { connectAuthEmulator, getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 
 const app = initializeApp(firebaseConfig);
@@ -15,6 +18,15 @@ const setAuthPersistence = async () => {
 
 setAuthPersistence();
 
+let appCheck: AppCheck;
+
+if (browser && env.PUBLIC_RECAPTCHA_SITE_KEY) {
+    appCheck = initializeAppCheck(app, {
+        provider: new ReCaptchaV3Provider(env.PUBLIC_RECAPTCHA_SITE_KEY),
+        isTokenAutoRefreshEnabled: true,
+    });
+}
+
 if (dev || firebaseEmulator) connectAuthEmulator(auth, "http://localhost:9099");
 
-export { app, auth };
+export { app, auth, appCheck };
