5ef3efbf0d0005e4182bbe2b95eb815e829cedad
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-12-24 22:42:14 +0800

    feat: initialise app check with recaptcha

--- src/lib/client/utils/firebase.ts
+++ src/lib/client/utils/firebase.ts
@@ -1,11 +1,19 @@
 import { dev } from "$app/environment";
 
 import { firebaseConfig, firebaseEmulator } from "$client/config/firebase";
+import { env } from "$env/dynamic/public";
 
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { connectAuthEmulator, getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 
 const app = initializeApp(firebaseConfig);
+
+initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(env.PUBLIC_RECAPTCHA_SITE_KEY),
+    isTokenAutoRefreshEnabled: true,
+});
+
 const auth = getAuth(app);
 
 const setAuthPersistence = async () => {
