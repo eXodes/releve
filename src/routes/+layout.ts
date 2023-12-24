667df186448589b0993447dc0a078ddf7dc9eb86
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-12-25 00:32:49 +0800

    fix: client app check

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,25 +1,21 @@
-import { dev } from "$app/environment";
+import { browser } from "$app/environment";
 import { env } from "$env/dynamic/public";
 
-import { firebaseEmulator } from "$client/config/firebase";
 import { app } from "$client/utils/firebase";
 
-import type { AppCheckTokenResult } from "@firebase/app-check";
 import { getToken, initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ data, setHeaders }) => {
-    if (!(dev || firebaseEmulator)) {
-        let appCheckResult: AppCheckTokenResult;
-
+    if (browser) {
         const appCheck = initializeAppCheck(app, {
             provider: new ReCaptchaV3Provider(env.PUBLIC_RECAPTCHA_SITE_KEY),
             isTokenAutoRefreshEnabled: true,
         });
 
         try {
-            appCheckResult = await getToken(appCheck, false);
+            const appCheckResult = await getToken(appCheck, false);
 
             setHeaders({
                 "X-Firebase-AppCheck": appCheckResult.token,
