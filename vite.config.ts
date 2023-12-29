ab1fbd96405a1e39a3d0e41d1c0a30f70fd35e47
Exo Des <9076458+eXodes@users.noreply.github.com>
2023-12-29 15:34:06 +0800

    feat: vite pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { defineConfig, loadEnv } from "vite";
+import colors from "tailwindcss/colors";
 
 const config = defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
@@ -23,6 +24,36 @@ const config = defineConfig(({ mode }) => {
             sveltekit(),
             SvelteKitPWA({
                 registerType: "autoUpdate",
+                includeAssets: ["favicon.png", "workflow-mark.png", "workflow-mark.svg"],
+                manifest: {
+                    name: "Releve",
+                    short_name: "Releve",
+                    theme_color: colors.rose["600"],
+                    background_color: colors.white,
+                    display: "standalone",
+                    icons: [
+                        {
+                            src: "workflow-mark-192.png",
+                            sizes: "192x192",
+                            type: "image/png",
+                        },
+                        {
+                            src: "workflow-mark-512.png",
+                            sizes: "512x512",
+                            type: "image/png",
+                        },
+                        {
+                            src: "workflow-mark-512.png",
+                            sizes: "512x512",
+                            type: "image/png",
+                            purpose: "maskable",
+                        },
+                    ],
+                },
+                workbox: {
+                    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+                    globIgnores: ["server/*.*", "server/sw.js", "server/workbox-*.js"],
+                },
             }),
         ],
     };
