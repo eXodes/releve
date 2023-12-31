90059d6c7584694152951adb3a3271fefb625bbe
Exo Des <9076458+eXodes@users.noreply.github.com>
2024-01-01 01:43:39 +0800

    test: layout pages fixtures

--- tests/fixtures/layout/pages/user.ts
+++ tests/fixtures/layout/pages/user.ts
@@ -3,15 +3,36 @@ import type { Locator, Page } from "$tests/utils";
 import { expect } from "@playwright/test";
 
 export class UserLayoutPage extends BaseLayoutPage {
-    private readonly navShopsEl: Locator;
-    private readonly navUsersEl: Locator;
-
     private readonly toastAddShopSuccessEl: Locator;
 
     constructor(public readonly page: Page) {
         super(page);
 
         this.toastAddShopSuccessEl = page.getByText(/Shop has been submitted for approval./);
+    }
+
+    async navigateToMyShops() {
+        await this.navMyShopsEl.waitFor();
+        await this.navMyShopsEl.click();
+
+        await this.page.waitForURL("/my/shops");
+    }
+
+    async navigateToShopsStatus() {
+        await this.navShopsStatusEl.waitFor();
+        await this.navShopsStatusEl.click();
+
+        await this.page.waitForURL("/my/status");
+    }
+
+    async navigateToShopsManagement() {
+        await this.navShopsEl.waitFor({ state: "detached" });
+        await expect(this.navShopsEl).not.toBeVisible();
+    }
+
+    async navigateToUsersManagement() {
+        await this.navUsersEl.waitFor({ state: "detached" });
+        await expect(this.navUsersEl).not.toBeVisible();
     }
 
     async saveNewShopSuccess() {
