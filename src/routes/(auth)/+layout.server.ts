a7a1a67a2b6a808fe844cf488056930b9caf87e1
Exo Des <9076458+eXodes@users.noreply.github.com>
2024-01-02 20:13:56 +0800

    feat: set redirect url when require auth

--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -2,9 +2,11 @@ import { redirect } from "@sveltejs/kit";
 
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = ({ locals, depends }) => {
+export const load: LayoutServerLoad = ({ locals, url, depends }) => {
+    const redirectUrl = url.searchParams.get("redirect");
+
     if (locals.session) {
-        redirect(303, "/");
+        redirect(303, redirectUrl ?? "/");
     }
 
     depends("session");
